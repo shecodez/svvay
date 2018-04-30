b9b3639333870a993b1f732572c7a0e50d11f3e0
shecodez - Nicole J. Nobles <shecodez@gmail.com>
2018-04-30 11:12:57 -0400

    Create backend using Spring Boot

---
+++ src/main/java/com/example/svvay/SvvayApplication.java
@@ -0,0 +1,12 @@
+package com.example.svvay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvvayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvvayApplication.class, args);
+	}
+}
